9f88bde4a206fd86c2b70d99ef66c529ca25aa45
ryan-dia <cjfdnjs158@naver.com>
2023-05-31 14:57:40 +0900

    feat(main): main 화면

---
+++ app/page.tsx
@@ -0,0 +1,35 @@
+import FloatingButton from '@/components/floating-button';
+import Item from '@/components/item';
+import Layout from '@/components/layout';
+import type { NextPage } from 'next';
+
+const Home: NextPage = () => {
+  return (
+    <Layout title='홈' hasTabBar>
+      <div className='flex flex-col items-end px-3 py-10 space-y-5 '>
+        {[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1].map((_, i) => (
+          <Item id={i} key={i} title='iPhone 14' price={99} comments={1} hearts={1} />
+        ))}
+        <FloatingButton href='/items/upload'>
+          <svg
+            className='w-6 h-6'
+            xmlns='http://www.w3.org/2000/svg'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='currentColor'
+            aria-hidden='true'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M12 6v6m0 0v6m0-6h6m-6 0H6'
+            />
+          </svg>
+        </FloatingButton>
+      </div>
+    </Layout>
+  );
+};
+
+export default Home;
